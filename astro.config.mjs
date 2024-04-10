2c6cd233ef77f6def2cb6912fb7ca9b24e210efb
swomf <swomf@proton.me>
2024-04-10 04:30:23 -0400

    feat: basic search bar for all blog posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
-
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	markdown: {
-		remarkPlugins: [remarkMath],
-		rehypePlugins: [rehypeKatex]
-	}
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), react()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex]
+  }
 });
---
+++ src/components/Search.tsx
@@ -0,0 +1,55 @@
+import Fuse from 'fuse.js';
+import { useState } from 'react';
+
+// Configs fuse.js
+// https://fusejs.io/api/options.html
+const options = {
+	keys: ['props.title', 'props.description'],
+	includeMatches: true,
+	minMatchCharLength: 2,
+	threshold: 0.5,
+};
+
+interface SearchProps {
+	searchList: Record<string, any>[]
+}
+
+function Search({ searchList }: SearchProps) {
+	// User's input
+	const [query, setQuery] = useState('');
+
+	const fuse = new Fuse(searchList, options);
+
+	// Set a limit to the posts: 5
+	const posts = fuse
+		.search(query)
+		.map((result) => result.item)
+		.slice(0, 5);
+
+	function handleOnSearch({ target }: React.ChangeEvent<HTMLInputElement>) {
+		setQuery(target.value);
+	}
+
+	return (
+		<>
+			<label>Search</label>
+			<input type="text" value={query} onChange={handleOnSearch} placeholder="Search posts" />
+			{query.length > 1 && (
+				<p>
+					Found {posts.length} {posts.length === 1 ? 'result' : 'results'} for '{query}'
+				</p>
+			)}
+			<ul>
+				{posts &&
+					posts.map((post) => (
+						<li key={post.props.title}>
+							<a href={`/blog/${post.params.slug}`}>{post.props.title}</a>
+							<p> {post.props.description} </p>
+						</li>
+					))}
+			</ul>
+		</>
+	);
+}
+
+export default Search;
