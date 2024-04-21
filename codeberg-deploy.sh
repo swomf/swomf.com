059eb17c6023ce1371ac2a53fc24e2a33b50b201
swomf <swomf@proton.me>
2024-04-21 01:43:33 -0400

    fix(build): codeberg-deploy.sh throws no errors

--- codeberg-deploy.sh
+++ codeberg-deploy.sh
@@ -1,11 +1,28 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-rm -rf dist
-git clone https://codeberg.org/swomf/pages dist
-rm -rf dist/*
+arrow_msg() {
+    echo "$(tput setaf 2)$(tput bold) => $(tput sgr0)$(tput bold)${1}$(tput sgr0)"
+}
+current_commit=$(git log --pretty=format:'%H' -n 1)
+
+arrow_msg "Building static directories"
 pnpm build
+
+arrow_msg "Making TEMPDIR"
+some_dir=$(mktemp -d)
+arrow_msg "Cloning to TEMPDIR"
+git clone --no-checkout https://codeberg.org/swomf/pages ${some_dir}
+if [ -d "dist" ]; then
+  arrow_msg "Removing dist/.git to replace with TEMPDIR/.git"
+  rm -rf dist/.git
+fi
+arrow_msg "Moving TEMPDIR/.git to dist/"
+mv ${some_dir}/.git dist/
+
+
+arrow_msg "Committing ${current_commit}"
 cd dist
 git add --all
-git commit -m "$(git log --pretty=format:'%H' -n 1)"
+git commit -m "${current_commit}"
 git push
