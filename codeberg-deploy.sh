5e4a0785849b0cc6dc590c4782f9e1572002a79f
swomf <swomf@proton.me>
2024-09-07 05:22:09 -0400

    build: codeberg-deploy.sh pulls with https, pushes with ssh
    Before this commit, the SSH password needed to be entered twice, during
    pull and push. Now, it only needs to be typed during push.
    
    This edit passes shellcheck.

--- codeberg-deploy.sh
+++ codeberg-deploy.sh
@@ -5,7 +5,8 @@ arrow_msg() {
   echo "$(tput setaf 2)$(tput bold) => $(tput sgr0)$(tput bold)${1}$(tput sgr0)"
 }
 current_commit=$(git log --pretty=format:'%H' -n 1)
-pages_push_location="git@codeberg.org:swomf/pages.git"
+pages_push_https="https://codeberg.org/swomf/pages.git"
+pages_push_ssh="git@codeberg.org:swomf/pages.git"
 pages_repo_dir=".codeberg-deploy.d"
 
 arrow_msg "Building static directories"
@@ -13,22 +14,22 @@ pnpm build
 
 if [[ -d "${pages_repo_dir}" && -d "${pages_repo_dir}/.git" ]]; then
   arrow_msg "Syncing ${pages_repo_dir} to origin/pages via reset"
-  cd ${pages_repo_dir}
-  git fetch
+  cd "${pages_repo_dir}"
+  git fetch "${pages_push_https}"
   git reset origin/pages &> /dev/null
   cd ..
 else
   arrow_msg "Syncing ${pages_repo_dir} to origin/pages via clone"
-  rm -rf ${pages_repo_dir}
-  git clone --no-checkout ${pages_push_location} ${pages_repo_dir}
+  rm -rf "${pages_repo_dir}"
+  git clone --no-checkout "${pages_push_https}" "${pages_repo_dir}"
 fi
 
 arrow_msg "Copying ${pages_repo_dir}/.git to dist/"
 rm -rf dist/.git
-cp -R ${pages_repo_dir}/.git dist/
+cp -R "${pages_repo_dir}/.git" dist/
 
 arrow_msg "Committing ${current_commit}"
 cd dist
 git add --all
 git commit -m "${current_commit}"
-git push
+git push "${pages_push_ssh}"
