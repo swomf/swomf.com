f0cf2d46fb5c8f5a3a4af9c8eabf3c838da5906d
swomf <swomf@proton.me>
2024-09-28 18:03:43 -0400

    build: codeberg-deploy.sh has --help and --force options
    * Use set -euo for undefined variable check

--- codeberg-deploy.sh
+++ codeberg-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -eo pipefail
+set -euo pipefail
 
 arrow_msg() {
   echo "$(tput setaf 2)$(tput bold) => $(tput sgr0)$(tput bold)${1}$(tput sgr0)"
@@ -8,6 +8,37 @@ current_commit=$(git log --pretty=format:'%H' -n 1)
 pages_push_https="https://codeberg.org/swomf/pages.git"
 pages_push_ssh="git@codeberg.org:swomf/pages.git"
 pages_repo_dir=".codeberg-deploy.d"
+
+usage() {
+cat << EOF
+Build and deploy an astro project to ${pages_push_https}
+
+Usage: $0 [OPTIONS]
+
+Options:
+  -h, --help    show this help.
+  -f, --force   force deploy to pages.
+EOF
+}
+
+push_opts=()
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -f|--force)
+      push_opts+=("--force")
+      shift
+      ;;
+    -h|--help)
+      usage
+      exit 0
+      ;;
+    *)
+      echo "Unknown parameter passed: $1"
+      exit 1
+      ;;
+  esac
+done
 
 arrow_msg "Building static directories"
 pnpm build
@@ -32,4 +63,11 @@ arrow_msg "Committing ${current_commit}"
 cd dist
 git add --all
 git commit -m "${current_commit}"
-git push "${pages_push_ssh}"
+
+if [[ -n "${push_opts[*]}" ]]; then
+  arrow_msg "Pushing with options: ${push_opts[*]}"
+  git push "${pages_push_ssh}" "${push_opts[@]}"
+else
+  arrow_msg "Pushing"
+  git push "${pages_push_ssh}"
+fi
