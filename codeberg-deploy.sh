39149a094662ff7694e7ab36f75865db5716c8dc
swomf <swomf@proton.me>
2024-11-25 16:08:01 -0500

    build: include .domains file in dist
    This is necessary for swomf.com DNS to function

--- codeberg-deploy.sh
+++ codeberg-deploy.sh
@@ -10,7 +10,7 @@ pages_push_ssh="git@codeberg.org:swomf/pages.git"
 pages_repo_dir=".codeberg-deploy.d"
 
 usage() {
-cat << EOF
+  cat <<EOF
 Build and deploy an astro project to ${pages_push_https}
 
 Usage: $0 [OPTIONS]
@@ -25,18 +25,18 @@ push_opts=()
 
 while [[ $# -gt 0 ]]; do
   case $1 in
-    -f|--force)
-      push_opts+=("--force")
-      shift
-      ;;
-    -h|--help)
-      usage
-      exit 0
-      ;;
-    *)
-      echo "Unknown parameter passed: $1"
-      exit 1
-      ;;
+  -f | --force)
+    push_opts+=("--force")
+    shift
+    ;;
+  -h | --help)
+    usage
+    exit 0
+    ;;
+  *)
+    echo "Unknown parameter passed: $1"
+    exit 1
+    ;;
   esac
 done
 
@@ -47,7 +47,7 @@ if [[ -d "${pages_repo_dir}" && -d "${pages_repo_dir}/.git" ]]; then
   arrow_msg "Syncing ${pages_repo_dir} to origin/pages via reset"
   cd "${pages_repo_dir}"
   git fetch "${pages_push_https}"
-  git reset origin/pages &> /dev/null
+  git reset origin/pages &>/dev/null
   cd ..
 else
   arrow_msg "Syncing ${pages_repo_dir} to origin/pages via clone"
@@ -58,6 +58,9 @@ fi
 arrow_msg "Copying ${pages_repo_dir}/.git to dist/"
 rm -rf dist/.git
 cp -R "${pages_repo_dir}/.git" dist/
+
+arrow_msg "Copying .domains file"
+cp domains dist/.domains
 
 arrow_msg "Committing ${current_commit}"
 cd dist
@@ -71,3 +74,4 @@ else
   arrow_msg "Pushing"
   git push "${pages_push_ssh}"
 fi
+
